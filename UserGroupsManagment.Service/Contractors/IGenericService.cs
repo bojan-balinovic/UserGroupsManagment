65d2d4f86802831ebda30ed885ee66f265010cc2
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-05 10:19:55 +0200

    add IGenericService interface

---
+++ UserGroupsManagment.Service/Contractors/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UserGroupsManagment.Model;
+
+namespace UserGroupsManagment.Service.Contractors
+{
+    public interface IGenericService<T, Filter>
+    {
+        Task<T> AddOne(T model);
+
+        Task<T> DeleteOne(Guid id);
+
+        Task<PaginationList<T>> GetMany(Filter filter, int currentPage = -1, string orderBy = "");
+
+        Task<T> GetOne(Guid id);
+
+        Task<T> UpdateOne(T model);
+        Task<IEnumerable<T>> GetAll();
+    }
+}
