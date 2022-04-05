0f01d4246fa00683597a265fadae071d5725ed7f
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-05 10:20:05 +0200

    add IUserInterface

---
+++ UserGroupsManagment.Service/Contractors/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserGroupsManagment.Model.Contractors;
+using UserGroupsManagment.Model.Filters;
+
+namespace UserGroupsManagment.Service.Contractors
+{
+    public interface IUserService:IGenericService<IUser, UserFilter>
+    {
+    }
+}
