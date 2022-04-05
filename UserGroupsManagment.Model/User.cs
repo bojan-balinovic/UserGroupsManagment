27aadd1ca0c9a72235bc7c309e63a32c3a6db4df
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-05 10:17:10 +0200

    add validation attributes

--- UserGroupsManagment.Model/User.cs
+++ UserGroupsManagment.Model/User.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using UserGroupsManagment.Model.Contractors;
 
 namespace UserGroupsManagment.Model
 {
     public class User:BaseModel, IUser
     {
+        [EmailAddress]
         public string Email { get; set; }
+        [MinLength(8)]
         public string Password { get; set; }
         public string Name { get; set; }
     }
