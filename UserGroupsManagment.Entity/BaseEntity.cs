d5eeaf6687c80fb507cb92c69d4c2ecc70fcc958
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-06 16:56:13 +0200

    add atribute

--- UserGroupsManagment.Entity/BaseEntity.cs
+++ UserGroupsManagment.Entity/BaseEntity.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace UserGroupsManagment.Entity
 {
     public class BaseEntity
     {
+        [Key]
         public int Id { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
