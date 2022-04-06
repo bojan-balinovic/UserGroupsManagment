5d66ea25516b904d83f355cf3983517fbd8dca05
_Bojan_ <bojanbalinovi@gmail.com>
2022-04-06 22:40:36 +0200

    add EditGroupViewModel.cs

---
+++ UserGroupsManagment/ViewModels/EditGroupViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserGroupsManagment.Model;
+
+namespace UserGroupsManagment.ViewModels
+{
+    public class EditGroupViewModel
+    {
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
