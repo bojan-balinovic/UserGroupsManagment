67a25447cdc77185a25d4fdee8c81c2981e21164
_Bojan_ <bojanbalinovi@gmail.com>
2022-04-06 22:38:35 +0200

    add props DateModified, DateCreated

--- UserGroupsManagment.Model/Contractors/IBaseModel.cs
+++ UserGroupsManagment.Model/Contractors/IBaseModel.cs
@@ -7,5 +7,7 @@ namespace UserGroupsManagment.Model.Contractors
     public interface IBaseModel
     {
         public int Id { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
