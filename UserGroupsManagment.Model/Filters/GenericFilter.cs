ecc31fdfb3a01f60bcad9569a38cabcb0849b3f3
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-06 16:57:16 +0200

    changed property type

--- UserGroupsManagment.Model/Filters/GenericFilter.cs
+++ UserGroupsManagment.Model/Filters/GenericFilter.cs
@@ -6,8 +6,7 @@ namespace UserGroupsManagment.Model.Filters
 {
     public abstract class GenericFilter
     {
-        public virtual Guid? Id { get; set; }
-        public Guid? UserId { get; set; }
+        public virtual int? Id { get; set; }
         public int? CurrentPage { get; set; }
         public int? PageSize { get; set; }
         public string OrderBy { get; set; }
