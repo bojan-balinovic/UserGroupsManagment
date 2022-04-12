0f0ab0784cc317f34c7b3fb411c9ed71e492cc8a
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-12 10:02:28 +0200

    fix

--- UserGroupsManagment.Model/Filters/GenericFilter.cs
+++ UserGroupsManagment.Model/Filters/GenericFilter.cs
@@ -7,7 +7,7 @@ namespace UserGroupsManagment.Model.Filters
     public abstract class GenericFilter
     {
         public virtual int? Id { get; set; }
-        public int? CurrentPage { get; set; }
+        public int? CurrentPage { get; set; } = 1;
         public int? PageSize { get; set; }
         public string OrderBy { get; set; }
     }
