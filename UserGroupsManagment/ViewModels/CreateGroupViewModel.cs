cd2b634bfc4d161a489c959d53dea56fdb7b350d
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-12 12:54:59 +0200

    added rquired attributes

--- UserGroupsManagment/ViewModels/CreateGroupViewModel.cs
+++ UserGroupsManagment/ViewModels/CreateGroupViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using UserGroupsManagment.Model;
@@ -9,6 +10,7 @@ namespace UserGroupsManagment.ViewModels
 {
     public class CreateGroupViewModel
     {
+        [Required]
         public string Name { get; set; }
         public List<CreateUserViewModel> Users { get; set; }
     }
--- UserGroupsManagment/ViewModels/CreateUserViewModel.cs
+++ UserGroupsManagment/ViewModels/CreateUserViewModel.cs
@@ -9,9 +9,12 @@ namespace UserGroupsManagment.ViewModels
     public class CreateUserViewModel
     {
         [EmailAddress]
+        [Required]
         public string Email { get; set; }
         [MinLength(8)]
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Name { get; set; }
         public int? GroupId { get; set; }
     }
