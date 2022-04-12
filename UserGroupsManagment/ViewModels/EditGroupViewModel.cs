f2a7325d9ad6a2e415c9d8fe2c19a707f24fc292
Bojan Balinovic <bojanbalinovi@gmail.com>
2022-04-12 09:42:15 +0200

    change EditGroupViewModel

--- UserGroupsManagment/ViewModels/EditGroupViewModel.cs
+++ UserGroupsManagment/ViewModels/EditGroupViewModel.cs
@@ -8,7 +8,8 @@ namespace UserGroupsManagment.ViewModels
 {
     public class EditGroupViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public List<User> Users { get; set; }
+        public List<EditUserViewModel> Users { get; set; }
     }
 }
